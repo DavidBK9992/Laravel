8db2a5535a55741498c3192e83ebadb6221cf655
David Köberl <davidkoberl@Davids-iMac.local>
2026-01-29 08:21:35 +0100

    Changed view of Jobs on URL/jobs

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,12 +1,14 @@
 <x-layout>
     <x-slot:heading>Jobs Page</x-slot:heading>
-    <h1>Hello, welcome to the Jobs Page! </h1>
 
 
-    <ul>
+
+
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-        <li> <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline"><strong>{{$job['title']}}:</strong> Pays {{ $job['salary']}} per year.</a> </li>
+        <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded"><strong>{{$job['title']}}:</strong> Pays {{ $job['salary']}} € per year.</a>
         @endforeach
-    </ul>
+    </div>
+
 
 </x-layout>
